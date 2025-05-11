a2e0102980d43fe3b45819b806052ba71db56eac
Dark <mohamed2003elashmawy@gmail.com>
2025-05-11 16:45:59 +0300

    Installed all required dependencies and connected to MongoDB server

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get('/', (req, res) => res.send('API is running'));
+
+
+// Connect to DB and start server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
